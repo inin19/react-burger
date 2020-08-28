a6223a541aebc3fa340a95483e292e575b303c6c
Yingying Chen <cyy870119@gmail.com>
2020-08-28 09:39:58 -0400

    burger

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuild';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 // import classes from './App.module.css';
 
 class App extends Component {
