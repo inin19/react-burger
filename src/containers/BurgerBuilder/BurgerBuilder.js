8f7c9798fdef0ebcbceafb738da9848875f73138
Yingying Chen <cyy870119@gmail.com>
2020-09-01 15:30:22 -0400

    with router

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,6 +26,8 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
+    console.log(this.props);
+
     axios
       .get('/ingredients.json')
       .then((response) => {
@@ -102,31 +104,31 @@ class BurgerBuilder extends Component {
   purchaseContinueHandler = () => {
     // alert('you continue');
     // todo
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'yy',
+    //     address: {
+    //       street: 'test 1',
+    //       zipCode: '07302',
+    //       country: 'USA',
+    //     },
+    //     email: 'cyy870119@gmail.com',
+    //   },
+    //   deliveryMethod: 'fastest',
+    // };
+    // axios
+    //   .post('/orders.json', order)
+    //   .then((response) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
 
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'yy',
-        address: {
-          street: 'test 1',
-          zipCode: '07302',
-          country: 'USA',
-        },
-        email: 'cyy870119@gmail.com',
-      },
-      deliveryMethod: 'fastest',
-    };
-
-    axios
-      .post('/orders.json', order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    this.props.history.push('/checkout');
   };
 
   purchaseHandler = () => {
