08f4076d9073f7192e20d0420ee2fbdd192708d9
Yingying Chen <cyy870119@gmail.com>
2020-09-02 12:58:17 -0400

    before form

---
+++ src/components/Order/Order.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import classes from './Order.module.css';
+
+const order = (props) => {
+  const ingredients = [];
+
+  for (let ingreidentName in props.ingredients) {
+    ingredients.push({
+      name: ingreidentName,
+      amount: props.ingredients[ingreidentName],
+    });
+  }
+
+  const ingredientOutput = ingredients.map((ig) => {
+    return (
+      <span
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: '5px',
+        }}
+        key={ig.name}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingridnets: {ingredientOutput} </p>
+      <p>
+        Price: <strong>{props.price.toFixed(2)}</strong>
+      </p>
+    </div>
+  );
+};
+
+export default order;
