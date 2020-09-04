adeae7a60194c6344576b601ebf957b9c837d4c3
Yingying Chen <cyy870119@gmail.com>
2020-09-03 22:26:47 -0400

    orders redux

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -27,10 +27,26 @@ const reducer = (state = initalState, action) => {
         ...state,
         loading: false,
         orders: state.orders.concat(newOrder),
-        purchased: true
+        purchased: true,
       };
     case actionTypes.PURCHASE_BURGER_FAIL:
       return { ...state, loading: false };
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading: false,
+      };
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return {
+        ...state,
+        loading: false,
+      };
     default:
       return state;
   }
