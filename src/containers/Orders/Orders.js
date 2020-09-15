60a15e6efaced931d325bf9af8cc466c2a5b5166
Yingying Chen <cyy870119@gmail.com>
2020-09-15 16:49:56 -0400

    route protected

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import Spinner from './../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
   componentDidMount() {
-    this.props.onFetchOrder(this.props.token);
+    this.props.onFetchOrder(this.props.token, this.props.userId);
   }
 
   render() {
@@ -37,12 +37,14 @@ const mapStateToProps = (state) => {
     orders: state.order.orders,
     loading: state.order.loading,
     token: state.auth.token,
+    userId: state.auth.userId,
   };
 };
 
 const mapDisPatchToProps = (dispatch) => {
   return {
-    onFetchOrder: (token) => dispatch(actions.fetchOrders(token)),
+    onFetchOrder: (token, userId) =>
+      dispatch(actions.fetchOrders(token, userId)),
   };
 };
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -16,6 +16,10 @@ export const authSuccess = (token, userId) => {
 };
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
+
   return {
     type: actionTypes.AUTH_LOGOUT,
   };
@@ -58,6 +62,15 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then((response) => {
         // console.log(response);
+
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        );
+
+        localStorage.setItem('token', response.data.idToken);
+        localStorage.setItem('userId', response.data.localId);
+        localStorage.setItem('expirationDate', expirationDate);
+
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -72,5 +85,34 @@ export const setAuthRedirectPath = (path) => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path,
+  };
+};
+
+export const authCheckState = () => {
+  console.log('check state');
+  return (dispatch) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+
+      console.log(expirationDate);
+      console.log(new Date());
+
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        console.log('here??');
+
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
+      }
+    }
   };
 };
